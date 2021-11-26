e38b08fe2d4fa8c3e0b1eafee721ad0e3678f382
“Srujan03076” <srujanmahaja5@gmail.com>
2021-11-26 21:16:25 +0530

    [Srujan]Added Java Script (JS) File for Employee Payroll

---
+++ js/new_payroll_form.js
@@ -0,0 +1,93 @@
+//UC2:On Document Load Set Event Listeners
+
+window.addEventListener('DOMContentLoaded',(event)=>
+{
+    const name=document.querySelector("#name");
+    const textError = document.querySelector(".text-error");
+    name.addEventListener('input', function () 
+    {
+        if(name.value.length == 0)
+        {
+            textError.textContent ="";
+            return;
+        }
+        try
+        {
+            (new EmployeePayrollData()).name=name.value;
+            textError.textContent ="";
+        }
+        catch(ex)
+        {
+            textError.textContent =ex;
+        }
+    });
+
+    const salary = document.querySelector("#salary");
+    const output = document.querySelector("#salary-output");
+    output.textContent = salary.value;
+    salary.addEventListener("input", function () {
+    output.textContent = salary.value;
+    });
+});
+
+//UC3:Ability to create Employee Payroll Object On Save
+
+const save = () => {
+    try {
+        let employeePayrollData= createEmployeePayroll();
+     } catch (e) {
+        return;
+    }
+}
+
+const createEmployeePayroll  = () => {
+    let employeePayrollData = new EmployeePayrollData;
+    try {
+         employeePayrollData.name = getInputValueById('#name');
+        } catch (e) {
+           setTextValue('.text.error',e);
+           throw e;
+        }
+        employeePayrollData.profilePic = getSelectedValues('[name=profile]').pop();
+        employeePayrollData.gender = getSelectedValues('[name=gender]').pop();
+        employeePayrollData.department = getSelectedValues('[name=department]');
+        employeePayrollData.salary = getInputValueById('#salary');
+        employeePayrollData.note = getInputValueById('#notes');
+        let date = getInputValueById('#day') + " " + getInputValueById('#month') + " " +
+        getInputValueById('#year');
+        employeePayrollData.date = Date.parse(date);
+        alert(employeePayrollData.toString());
+        return employeePayrollData;
+}
+
+const getSelectedValues = (propertyValue) => {
+    let allItems = document.querySelectorAll(propertyValue);
+    let selItems = [];
+    allItems.forEach(item => {
+        if (item.checked) selItems.push(item.value);
+    });
+    return selItems;
+}
+
+/*
+ * 1: querySelector is the newer feature.
+ * 2: The querySelector method can be used when selecting by element name, 
+ *    nesting, or class name.
+ * 3: querySelector lets you find elements with rules that can't be 
+ *    expressed with getElementById
+ */
+const getInputValueById = (id) => {
+    let value = document.querySelector(id).value;
+    return value;
+}
+
+/*
+ * 1: getElementById is better supported than querySelector in older versions
+ *    of the browsers.
+ * 2: The thing with getElementById is that it only allows to select an 
+ *    element by its id.  
+ */
+const getInputElementValue = (id) => {
+    let value = document.getElementById(id).value;
+    return value;
+}
