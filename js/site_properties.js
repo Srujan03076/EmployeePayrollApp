bf7c40a7edd2a801773e755007ce65ef582d4ee6
“Srujan03076” <srujanmahaja5@gmail.com>
2021-11-28 18:23:45 +0530

    [Srujan]Added Java Script (JS) File for Employee Payroll

---
+++ js/site_properties.js
@@ -0,0 +1,4 @@
+let site_properties ={
+    home_page: "C:\Users\Admin\Desktop\EmpPayrollApp\pages\new_home.html",
+    add_emp_payroll_page: "C:\Users\Admin\Desktop\EmpPayrollApp\pages\new_employee_payroll.html"
+};
